257420d2b48bb2f0ca927b3b971101773c1ab738
squirrl <niko.tzikas@gmail.com>
2017-06-02 09:43:05 -0400

    chjgjhm

--- src/js/client.js
+++ src/js/client.js
@@ -1,4 +1,29 @@
+
 import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+
+import reducer from './reducers/reducer'
+import Layout from "./components/Layout";
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+const app = document.getElementById('app')
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+	<Provider store={store}>
+		<MuiThemeProvider>	
+			<Layout/>
+		</MuiThemeProvider>
+	</Provider>
+	, app);
+
+
+/*import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from 'redux'
 import {Provider} from 'react-redux'
@@ -56,7 +81,7 @@ import SimpleMapExample from './components/Map/Map';
 //import { Router, Route, browserHistory } from 'react-router'
 //import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
-
+/*
 
 const app = document.getElementById('app')
 
@@ -93,4 +118,4 @@ ReactDOM.render(
     
   </Router>
 		
-	, app);
+	, app);*/
